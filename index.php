e3781d9f41d72edb2da9b177f9cf53b3a03c91a0
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-07-21 21:29:37 +0530

    add first calculation in php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<h1>hello world</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<title>Learn in html</title>
+
+<body>
+    <h1>comments in PHP</h1>
+
+    <?php
+    echo 25 + 56;
+    ?>
+</body>
+</body>
+
+</html>
