3371f69e798ae7a27a1cd18a7a51e7eb4672bdf6
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-07-23 15:04:03 +0530

    array

---
+++ array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<title>Learn in html</title>
+
+<body>
+    <h1 style="text-align: center; font-size:2cm">Array in PHP</h1>
+
+    $jar = ["candies", "flowers",11, true];
+   
+</body>
+
+</html>
